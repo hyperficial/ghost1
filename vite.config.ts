603a8a5c5a3b2d64f87859516478920f9758a8fd
Owski <149725877+hyperficial@users.noreply.github.com>
2025-05-16 18:59:21 -0500

    HHH

--- vite.config.ts
+++ vite.config.ts
@@ -9,41 +9,41 @@ import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
 import wisp from "wisp-server-node";
 
 export default defineConfig({
-	root: "./src",
-	build: {
-		outDir: "../dist",
-		emptyOutDir: true,
-	},
-	plugins: [
-		ViteMinifyPlugin(),
-		viteStaticCopy({
-			targets: [
-				{
-					src: `${uvPath}/**/*`.replace(/\\/g, "/"),
-					dest: "uv",
-					overwrite: false,
-				},
-				{
-					src: `${baremuxPath}/**/*`.replace(/\\/g, "/"),
-					dest: "bare-mux",
-					overwrite: false,
-				},
-				{
-					src: `${epoxyPath}/**/*`.replace(/\\/g, "/"),
-					dest: "epoxy",
-					overwrite: false,
-				},
-			],
-		}),
-		{
-			name: "Wisp Server",
-			configureServer(server) {
-				server.httpServer?.on("upgrade", (req, socket, head) => {
-					if (req.url?.startsWith("/wisp/")) {
-						wisp.routeRequest(req, socket, head);
-					}
-				});
-			},
-		},
-	],
+    root: "./src",
+    build: {
+        outDir: "../dist",
+        emptyOutDir: true,
+    },
+    plugins: [
+        ViteMinifyPlugin(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: `${uvPath}/**/*`.replace(/\\/g, "/"),
+                    dest: "uv",
+                    overwrite: false,
+                },
+                {
+                    src: `${baremuxPath}/**/*`.replace(/\\/g, "/"),
+                    dest: "bare-mux",
+                    overwrite: false,
+                },
+                {
+                    src: `${epoxyPath}/**/*`.replace(/\\/g, "/"),
+                    dest: "epoxy",
+                    overwrite: false,
+                },
+            ],
+        }),
+        {
+            name: "Wisp Server",
+            configureServer(server) {
+                server.httpServer?.on("upgrade", (req, socket, head) => {
+                    if (req.url?.startsWith("/wisp/")) {
+                        wisp.routeRequest(req, socket, head);
+                    }
+                });
+            },
+        },
+    ],
 });
